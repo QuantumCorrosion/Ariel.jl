643a5083321de7834fbd5efc9ce216fc4416e0e8
Jarvist Moore Frost <jarvist@gmail.com>
2018-08-21 14:50:58 +0100

    Start on a quote.

--- src/Ariel.jl
+++ src/Ariel.jl
@@ -1,5 +1,9 @@
 module Ariel
 
+println("Ariel.jl")
+println("Of sulphurous roaring the most mighty Neptune")
+println("Seem to besiege and make his bold waves tremble,")
+
 # package code goes here
 
 end # module
